c9f54949e71e324d1bbc0f8df2c4bb37a55db88f
dylyou <dyluri007@gmail.com>
2023-04-15 21:05:36 -0700

    made code for displaying the board and initializing array of chesspieces, basic functionallity added as well

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,128 @@
 #include "Board.hpp"
+#include "chesspieces/Chesspiece.hpp"
+#include "chesspieces/Chesspiece.cpp"
+#include <iostream>
+using namespace std;
+
+Board:: Board(){
+    whiteTurn = true; 
+    moves = 0; 
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            playArea[i][j] = Chesspiece(); 
+        }
+        
+    }
+    //Its likely all the following code will be changed except for the initializing team names. The rest will probably have to use the consttructors
+    //From each of the specific piece classes. Likely a lot of changes in the futur. 
+    for (int i = 0; i < 8; i++)
+    {
+        playArea[0][i].setTeam('W');
+        playArea[1][i].setTeam('W');
+        playArea[1][i].setName("Pawnn");
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        playArea[7][i].setTeam('B');
+
+        playArea[6][i].setTeam('B');
+        playArea[6][i].setName("Pawnn");
+    }
+    //Like this will probably for sure be gone (but keeping the names all 5 letters i think is a good touch);
+    playArea[0][0].setName("Rookk");
+    playArea[7][0].setName("Rookk");
+    playArea[0][1].setName("Knite");
+    playArea[7][1].setName("Knite");
+    playArea[0][2].setName("Bihop");
+    playArea[7][2].setName("Bihop");
+    playArea[0][3].setName("Queen");
+    playArea[7][3].setName("Queen");
+    playArea[0][4].setName("Kingg");
+    playArea[7][4].setName("Kingg");
+    playArea[0][5].setName("Bihop");
+    playArea[7][5].setName("Bihop");
+    playArea[0][6].setName("Knite");
+    playArea[7][6].setName("Knite");
+    playArea[0][7].setName("Rookk");
+    playArea[7][7].setName("Rookk"); 
+}
+void Board:: turn(){
+    if(whiteTurn){
+        cout <<"It is White's turn" << endl;
+    }
+    else{
+        cout << "It is Black's turn" << endl; 
+    }
+    int ogx = 0, ogy = 0;
+    int x = 0,y = 0;
+
+
+    cout<< "Which piece would you like to move? Enter the x axis first(1-8)" << endl;
+    while( ogx<1 || ogx>8){
+        cin >> ogx;
+    }
+    ogx-=1;
+    cout << "Enter the y axis now" << endl;
+    while( ogy<1 || ogy>8){
+        cin >> ogy;
+    }
+    ogy-=1;
+    
+
+    cout << "You have selected " << playArea[ogx][ogy].getName() << " on team " << playArea[ogx][ogy].getTeam() << endl;
+    
+
+    cout<< "Where would you like to move? Enter the x axis first (1-8)" << endl;
+    while( x<1 || x>8){
+        cin >> x;
+    }
+    x -=1;
+    cout << "Enter the y axis now" << endl;
+    while( y<1 || y>8){
+        cin >> y;
+    }
+    y-=1;
+    cout<< "You have chosen " << x+1 << "," << y+1 << endl; 
+    //Here would be code that moves stuff around, checks if king is moving into wrong spot, handles captures, etc.
+    //I imagine this function will be changed a LOT from what it is now. 
+    //Idea checklist:
+    //Check if occupied by opponent or team piece(return false, might have to turn this into a bool function or something, or make one)
+    //Check if valid move based on piece
+    // If empty, swap pieces
+    // If occupied by enemy, make enemy default constructor again (or destroy it somehow), and then swap
+    
+    moves++;
+    swapTurn();
+
+    
+}
+void Board:: swapTurn(){
+    //Might not change much either
+    if(whiteTurn){
+        whiteTurn = false;
+    }
+    else{
+        whiteTurn = true;
+    }
+}
+void Board:: display(){
+    //This display code I dont imagine changing much
+    for (int row = 7; row >=0; row--)
+    {   
+        cout << row + 1 <<"    "; 
+        for (int col = 0; col <8; col++)
+        {
+            cout << playArea[row][col].getTeam() << playArea[row][col].getName() << "\t\t";
+        }
+        cout << endl << endl;
+    }
+    cout << "    ";
+    for (int i = 0; i < 8; i++)
+    {
+        cout << "  " << i+1<< "  " << "\t\t";
+    }
+    cout << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "Board.hpp"
 using namespace std;
 
 int main(){ 
-    cout << "hi ";
-    return 0; 
+    cout << "hi " << endl;
+    // Chesspiece test;
+    // cout << test.getName() << endl; 
+    Board game;
+    while(game.getMoves() < 50){
+        game.display();
+        game.turn();
+    }
+
     cout << "Testing out the source control that vscode has. Nice job team!";
+    return 0; 
 }
