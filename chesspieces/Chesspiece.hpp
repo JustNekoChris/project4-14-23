f45153315ed021f8aeaf9e333e56a1ceb6ddee8c
dylyou <dyluri007@gmail.com>
2023-04-20 16:27:42 -0700

    added x and y coords to chesspiece class

--- chesspieces/Chesspiece.hpp
+++ chesspieces/Chesspiece.hpp
@@ -8,7 +8,11 @@ class Chesspiece{
         Chesspiece(char team, std::string name);
         void setInfo(char team, std::string name);
         //The setterss could be 1 function ngl 
+        void setPosX(int x);
+        void setPosY(int y);
 
+        int getPosX()const{return posX;}
+        int getPosY()const{return posY;}
         std:: string getName()const{return name;}
         char getTeam()const{return team;}
         virtual bool MoveCheck(int ogRow, int ogCol, int tRow, int tCol, Chesspiece* playArea[][8]){
@@ -21,6 +25,7 @@ class Chesspiece{
         char team; // B = black, W = white, N = neither (empty space);
         std::string name; // name of the piece (mostly going to be for debugging)
         bool isThreatened;
+        int posX, posY;
 
 
 };
