ede8ef09843cd7370d999cd43a301dd08a010ed5
Christian Barajas <chbarajas20@gmail.com>
2023-04-26 09:44:10 -0700

    bool movecheck made, not tested

--- chesspieces/Bishop/Bishop.cpp
+++ chesspieces/Bishop/Bishop.cpp
@@ -1,1 +1,89 @@
 #include "Bishop.hpp"
+#include <cmath>
+
+Bishop::Bishop(char team)
+{
+    name = "Bishop";
+    this->team = team; 
+}
+
+bool Bishop::MoveCheck(int ogRow, int ogCol, int tRow, int tCol, Chesspiece* playArea[][8])
+{
+    if(playArea[ogCol][ogRow]->getTeam() == 'W')
+    {
+        char team = 'W';
+    }
+    else
+    {
+        char team = 'B';
+    }
+
+    if(abs(tRow - ogRow) == abs(tCol - ogCol))
+    {
+        int difX = tCol - ogCol, difY = tRow- ogRow;
+        if(difX > 0 && difY > 0) // Check up right
+        {
+            int num = abs(difX);
+            for(int i = 0; i < num; i++)
+            {
+                if(playArea[ogCol + i][ogRow + i] == nullptr)
+                {}
+                else if(playArea[ogCol + i][ogRow + i]->getTeam() == team)
+                {
+                    return false;
+                }                
+            }
+            return true;
+        }
+        else if((difX < 0 && difY > 0)) // Check up left
+        {
+            int num = abs(difX);
+            for(int i = 0; i < num; i++)
+            {
+                if(playArea[ogCol - i][ogRow + i] == nullptr)
+                {}
+                else if(playArea[ogCol - i][ogRow + i]->getTeam() == team)
+                {
+                    return false;
+                }                
+            }
+            return true;
+        }
+        else if((difX < 0 && difY < 0)) // Check down left
+        {
+            int num = abs(difX);
+            for(int i = 0; i < num; i++)
+            {
+                if(playArea[ogCol - i][ogRow - i] == nullptr)
+                {}
+                else if(playArea[ogCol - i][ogRow - i]->getTeam() == team)
+                {
+                    return false;
+                }                
+            }
+            return true;
+        }
+        else if((difX > 0 && difY < 0)) // Check down right
+        {
+            int num = abs(difX);
+            for(int i = 0; i < num; i++)
+            {
+                if(playArea[ogCol + i][ogRow - i] == nullptr)
+                {}
+                else if(playArea[ogCol + i][ogRow - i]->getTeam() == team)
+                {
+                    return false;
+                }                
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
--- chesspieces/Bishop/Bishop.hpp
+++ chesspieces/Bishop/Bishop.hpp
@@ -3,8 +3,13 @@
 #include "../Chesspiece.hpp"
 #include "../Chesspiece.hpp"
 #include "../../Board.hpp"
-class Bishop 
+
+class Bishop : public Chesspiece
 {
-    
+    public:
+        Bishop(char team);
+        bool MoveCheck(int ogRow, int ogCol, int tRow, int tCol, Chesspiece* playArea[][8]) override;
+    protected:
+
 };
 #endif
