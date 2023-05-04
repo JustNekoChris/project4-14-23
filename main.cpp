89f28925daed8d1fed76b791ca5f1f90626c6657
dylyou <dyluri007@gmail.com>
2023-05-04 15:50:41 -0700

    basic code layout setup, reworks are required

--- main.cpp
+++ main.cpp
@@ -32,6 +32,37 @@ int main()
     //Window
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "Minimalistic Chess", sf:: Style:: Default);
     sf::Event event;
+    //Text
+    sf::Font font;
+    if (!font.loadFromFile("src/Poppins/Poppins-Thin.ttf"))
+    {
+        cout << "Error on getting fonts" << endl;
+        system("pause");
+    }
+    sf::Text text("Welcome to chess.", font, 30);
+    text.setFillColor(sf::Color::White);
+    text.setStyle(sf::Text::Bold);
+    text.setPosition(50, 25);
+    //Background
+    sf::RectangleShape rect(sf::Vector2f(1000, 1000)); // create a rectangle shape with dimensions 1000x1000
+    sf::Color color1(64, 64, 64); // dark grey color
+    sf:: Color black(0,0,0);
+    sf:: Color white(255,255,255);
+    
+    sf::Color color2(192, 192, 192); // dark white color
+    rect.setFillColor(color1);
+    //Chessboard sprite
+    bool turn;
+    sf::Texture background; 
+    if(!background.loadFromFile("src/images/Chess_Board.png")){
+        cout << "Load failed" << endl;
+        system("pause");
+    }
+
+    //Moving chessboard to the proper spot;
+    sf::Sprite board_sprite;
+    board_sprite.setPosition(100,100);
+    board_sprite.setTexture(background);
     //Game Loop
     while (window.isOpen())
     {
@@ -43,9 +74,21 @@ int main()
                 window.close();
                 break; 
             case sf::Event::MouseButtonPressed:
+
+                if(turn){
+                    rect.setFillColor(color1);
+                    text.setFillColor(white);
+                    turn = false;
+                }
+                else{
+                    rect.setFillColor(color2);
+                    text.setFillColor(black);
+                    turn = true; 
+                }
                 break;
                 
             }
+
             
         }
         //Game Updates 
@@ -66,7 +109,13 @@ int main()
 
         //Render window
         window.clear();
+        window.draw(rect);
+        window.draw(board_sprite);
+        window.draw(text);
         window.display();
+
     }
+    game.deleter();
+    cout << "Game ended!";
     return 0;
 }
